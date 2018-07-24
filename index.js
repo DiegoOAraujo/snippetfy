390dd9e472e4ad46538f1653cc3cf173ff8ad0e1
Diego Araujo <doasalah@gmail.com>
2018-07-24 12:58:17 -0300

    iniciando projeto

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+app.set('view engine', 'njk');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
