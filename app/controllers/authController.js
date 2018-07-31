18c38aa89d4677344f3cc1070f0b319d4e866f6b
Diego Araujo <doasalah@gmail.com>
2018-07-31 12:58:52 -0300

    adicionando novo usuario, criptografando senha com bcryptjs

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+
 module.exports = {
   signin(req, res) {
     return res.render('auth/signin');
@@ -6,5 +9,23 @@ module.exports = {
   signup(req, res) {
     return res.render('auth/signup');
   },
-  signout(req, res) { },
+
+  // signout(req, res) { },
+
+  async register(req, res) {
+    const { email } = req.body;
+    if (await User.findOne({ where: { email } })) {
+      return res.redirect('back');
+    }
+
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    // ...req.body, password
+    // é = a { nome, email, password, password }
+    // mas como só pode haver 1 password ele mata o primeiro
+
+    await User.create({ ...req.body, password });
+
+    return res.redirect('/');
+  },
 };
--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,16 @@ const routes = express.Router();
 
 const authController = require('./controllers/authController');
 
+/** métodos de route
+ * GET - pega informação
+ * POST - salva, criando
+ * PUT - altera, editação
+ * DELETE - adivinha?
+ */
+
 routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
+
+routes.post('/register', authController.register);
 
 module.exports = routes;
